22ddfdcf9dd2f5cda57ee7de1e4bf5a848d4857e
N M <nmusaelian@rallydev.com>
2013-09-17 16:48:01 -0600

    rewrote the app to build a grid with columns of Resolution values

--- src/App.js
+++ src/App.js
@@ -9,11 +9,41 @@ Ext.define('CustomApp', {
     },
 
     addContent: function() {
-        console.log('_makeStore');
-        this._makeStore();
+        this._arr = [];
+        var that = this;
+        console.log('_addContent');
+        
+        Rally.data.ModelFactory.getModel({
+            type: 'Defect',
+            context: {
+                workspace: '/workspace/12352608129'
+            },
+            success: function(model) {
+                that._allowedValuesStore = model.getField( 'Resolution' ).getAllowedValueStore( );
+                console.log("allowed values count", that._allowedValuesStore.getCount());
+                that._getDropdownValues();
+                that._makeStore();
+            }
+        });
     },
-    
+
+    _getDropdownValues: function(){
+        var that = this;
+        this._allowedValuesStore.load({
+        scope: this,
+        callback: function(records, operation, success){
+            console.log(records[1].get('StringValue'));
+            Ext.Array.each(records, function(val) {
+                            var s = val.get('StringValue');
+                            that._arr.push(s);
+            });
+            console.log("arr[2]", this._arr[2])
+            }
+        });
+    },
+
     _makeStore: function(){
+                      
          console.log('_makeStore');
          var filter = Ext.create('Rally.data.QueryFilter', {
                                 property: 'Priority',
@@ -36,7 +66,8 @@ Ext.define('CustomApp', {
                             filter = filter.and(this.getContext().getTimeboxScope().getQueryFilter());
                             
                             filter.toString();
-                            
+         
+                           
                             
          Ext.create('Rally.data.WsapiDataStore', {
             model: 'Defect',
@@ -59,54 +90,106 @@ Ext.define('CustomApp', {
     _onDataLoaded: function(store, data){
                 console.log('_onDataLoaded');
                 var defects = [];
-                var pendingTasks = data.length;
-                if (data.length ===0) {
-                    this._createGrid(defects);  //to force refresh on testset grid when there are no testsets in the iteration
+                if (data.length === 0) {
+                    this._createGrid(defects);  //to force refresh on testset grid when there are no items in the iteration
                 }
+
+                var countArchitecture=0;
+                var countCodeChange=0;
+                var countNotADefect=0;
+                var countNone = 0;
+                var countConfigurationChange =0;
+                var countDatabaseChange = 0;
+                var countDuplicate = 0;
+                var countNeedMoreInformation =0;
+                var countSoftwareLimitation =0;
+                var countUserInterface = 0;
+                
+                
                 Ext.Array.each(data, function(defect) {
-                            var d  = {
-                                FormattedID: defect.get('FormattedID'),
-                                Name: defect.get('Name'),
-                                Priority: defect.get('Priority'),
-                                State: defect.get('State'),
-                                Resolution: defect.get('Resolution'),
-                                _ref: defect.get("_ref"),
-                            };
                             
-                            defects.push(d);
-                            this._createGrid(defects);
-                }, this);
+                            var resolution = defect.get('Resolution');
+                            switch(resolution)
+                            {
+                            case "Architecture":
+                              countArchitecture++;
+                              break;
+                            case "Code Change":
+                              countCodeChange++;
+                              break;
+                            case "Not a Defect":
+                              countNotADefect++;
+                              break;
+                            case "None":
+                              countNone++;
+                              break;
+                            case "Configuration Change":
+                              countConfigurationChange++;
+                              break;
+                            case "Database Change":
+                              countDatabaseChange++;
+                              break;
+                            case "Duplicate":
+                              countDuplicate++;
+                              break;
+                            case "Need More Information":
+                              countDuplicate++;
+                              break;
+                            case "Software Limitation":
+                              countSoftwareLimitation++;
+                              break;
+                            case "User Interface":
+                              countUserInterface++;
+                              break;
+                            default:
+                              countNone++;
+                            }
+                });
+                   
+                            var d  = {
+                                'Architecture': countArchitecture,
+                                'Code Change': countCodeChange,
+                                'Not a Defect' : countNotADefect,
+                                '' : countNone, 
+                                'Configuration Change': countConfigurationChange,
+                                'Database Change': countDatabaseChange,
+                                'Duplicate': countDuplicate,
+                                'Need More Information': countNeedMoreInformation,
+                                'Software Limitation': countSoftwareLimitation,
+                                'User Interface':countUserInterface,
+                            };
+
+                           defects.push(d);
+                           this._createGrid(defects);         
     },
     
     _createGrid: function(defects) {
         console.log('_createGrid');
+        console.log('this._arr[3]', this._arr[3]);
+        var that = this;
+        
         var myStore = Ext.create('Rally.data.custom.Store', {
                 data: defects,
                 pageSize: 100,  
             });
+        
+        var columnConfig = [];
+                   
+                for (var i=0;i<this._arr.length; i++) {
+                    var columnConfigElement = {}; 
+                    columnConfigElement['text'] = that._arr[i];
+                    columnConfigElement['dataIndex'] = that._arr[i];
+                    columnConfig.push(columnConfigElement);
+                }
+        
+        console.log('columnConfig', columnConfig);
+        
         if (!this.grid) {
         this.grid = this.add({
             xtype: 'rallygrid',
             itemId: 'mygrid',
             store: myStore,
-            columnCfgs: [
-                {
-                   text: 'Formatted ID', dataIndex: 'FormattedID', xtype: 'templatecolumn',
-                    tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate')
-                },
-                {
-                    text: 'Name', dataIndex: 'Name'
-                },
-                {
-                    text: 'Priority', dataIndex: 'Priority'
-                },
-                {
-                    text: 'State', dataIndex: 'State'
-                },
-                {
-                    text: 'Resolution', dataIndex: 'Resolution'
-                }
-            ]
+            columnCfgs: columnConfig
         });
          
          }else{
